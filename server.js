f0781e1c9399a5074d596a944a31cc589c06f1cc
songdae jeong <66458347+songdae@users.noreply.github.com>
2021-09-14 11:02:32 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,60 @@
+const WebSocket = require('ws');
+const express = require('express');
+
+const app = express();
+
+const server = app.listen(3001, () => {
+    console.log('Start Server : localhost : 3001');
+});
+
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws, req) => { // 웹 소켓 연결 시
+    console.log('새로운 클라이언트 접속');
+    ws.on('message', (message) => { // 클라이언트로부터 메시지 수신 시
+        dat = message.toString();
+        dat_json = JSON.parse(dat);
+        //if (dat_json.type == 5)  // type이 5 = connect 일때
+        //{
+        //    wss.broadcast(dat,ws);
+        //}
+        wss.broadcast(dat,ws);
+    });
+
+    ws.on('error', (err) => { // 에러 발생 시
+        console.error(err);
+    });
+
+    ws.on('close', () => { // 연결 종료 시
+        wss.checkDisconnect(dat, ws);
+        //console.log(wss.clients);
+        console.log('클라이언트 접속 해제');
+        clearInterval(ws.interval);
+    });
+
+    // ws.interval = setInterval(() => {
+    //     if (ws.readyState === ws.OPEN) {
+    //         ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
+    //     }
+    // }, 3000);
+});
+
+
+wss.broadcast = function broadcast(data, sender) {
+    //console.log(data);
+    wss.clients.forEach(function each(client) {
+        if (client !== sender)
+        {
+            client.send(data);
+        }
+            
+     });
+};
+
+wss.checkDisconnect = function checkDisconnect(data, sender)
+{
+    wss.clients.forEach(function each(client){
+        console.log(client);
+    });
+}
