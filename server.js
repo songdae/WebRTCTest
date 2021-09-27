3195f7fbb8f2c81022bebef081d0136e5dd1435a
songdae <songdae_jeong@tmax.co.kr>
2021-09-27 11:41:45 +0900

    To socketIO

--- server.js
+++ server.js
@@ -1,64 +1,98 @@
-const WebSocket = require('ws');
-const express = require('express');
-
-const app = express();
-
-const server = app.listen(3001, () => {
-    console.log('Start Server : port 3001');
+const io = require('socket.io')(3100, {
+    pingInterval: 30005,
+    pingTimeout: 5000,
+    upgradeTimeout: 3000,
+    allowUpgrades: true,
+    cookie: false,
+    serveClient: true
 });
 
-
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws, req) => { // 웹 소켓 연결 시
-    console.log('새로운 클라이언트 접속');
-    ws.on('message', (message) => { // 클라이언트로부터 메시지 수신 시
-        dat = message.toString();
-        dat_json = JSON.parse(dat);
-        console.log(dat);
-
-        
-
-        wss.broadcast(dat,ws);
-        //ws.send(dat);
-        
-        //ws.send(dat);
-        //wss.broadcast(dat,ws);
-        //wss.clients.forEach(function each(client)
-        //{
-        //    if (client !== ws && client.readyState === WebSocket.OPEN)
-        //        client.send(dat);
-        //});
-
-    });
-
-    ws.on('error', (err) => { // 에러 발생 시
-        console.error(err);
-    });
-
-    ws.on('close', (reason) => { // 연결 종료 시
-        console.log('클라이언트 접속 해제');
-        console.log(reason);
-        clearInterval(ws.interval);
-    });
-});
+let users = {};
+let socketToRoom = {};
+const maximum = 2;
 
 
+console.log('Starting Socket.IO server, port : 3100');
 
-wss.broadcast = function broadcast(data, sender) {
-    //console.log(data);
-    wss.clients.forEach(function each(client) {
-        if (client !== sender && client.readyState === WebSocket.OPEN)
-        {
-            client.send(data);
+io.on('connection', (socket) => {
+	console.log('connected');
+	socket.on('join_room', data => {
+        console.log(socket.id);
+        // user[room]에는 room에 있는 사용자들이 배열 형태로 저장된다.
+        // room이 존재한다면
+        if (users[data.room]) {
+            const length = users[data.room].length;
+            // 최대 인원을 충족시켰으면 더 이상 접속 불가
+            if (length === maximum) {
+                socket.to(socket.id).emit('room_full');
+                return;
+            }
+            // 인원이 최대 인원보다 적으면 접속 가능
+            users[data.room].push({id: socket.id});
+            //users[data.room].push({id: socket.id, username : data.user_name});
+        } else { // room이 존재하지 않는다면 새로 생성
+            users[data.room] = [{id: socket.id}];
         }
-    });
-};
+        // 해당 소켓이 어느 room에 속해있는 지 알기 위해 저장
+        socketToRoom[socket.id] = data.room;
 
+        socket.join(data.room);
+        console.log(`[${socketToRoom[socket.id]}]: ${socket.id} enter`);
 
-wss.checkDisconnect = function checkDisconnect(data, sender)
-{
-    wss.clients.forEach(function each(client){
-        console.log(client);
+        // 본인을 제외한 같은 room의 user array
+        const usersInThisRoom = users[data.room].filter(user => user.id !== socket.id);
+
+        console.log(usersInThisRoom);
+
+        // 본인에게 해당 user array를 전송
+        // 새로 접속하는 user가 이미 방에 있는 user들에게 offer(signal)를 보내기 위해
+        io.sockets.to(socket.id).emit('all_users', usersInThisRoom);
+
+        socket.broadcast.emit('callee');
+
     });
-}
+
+	// 다른 user들에게 offer를 보냄 (자신의 RTCSessionDescription)
+    socket.on('offer', sdp => {
+        console.log('offer: ' + socket.id);
+        console.log(sdp);
+        //dat_json = JSON.parse(sdp.toString());
+        //console.log(dat_json);
+        // room에는 두 명 밖에 없으므로 broadcast 사용해서 전달
+        socket.broadcast.emit('getOffer', sdp);
+    });
+
+    // offer를 보낸 user에게 answer을 보냄 (자신의 RTCSessionDescription)
+    socket.on('answer', sdp => {
+        console.log('answer: ' + socket.id);
+        console.log(sdp);
+        // room에는 두 명 밖에 없으므로 broadcast 사용해서 전달
+        socket.broadcast.emit('getAnswer', sdp);
+    });
+
+    // 자신의 ICECandidate 정보를 signal(offer 또는 answer)을 주고 받은 상대에게 전달
+    socket.on('candidate', candidate => {
+        console.log('candidate: ' + socket.id);
+        // room에는 두 명 밖에 없으므로 broadcast 사용해서 전달
+        socket.broadcast.emit('getCandidate', candidate);
+    })
+
+    // user가 연결이 끊겼을 때 처리
+    socket.on('disconnect', () => {
+        console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
+        // disconnect한 user가 포함된 roomID
+        const roomID = socketToRoom[socket.id];
+        // room에 포함된 유저
+        let room = users[roomID];
+        // room이 존재한다면(user들이 포함된)
+        if (room) {
+            // disconnect user를 제외
+            room = room.filter(user => user.id !== socket.id);
+            users[roomID] = room;
+        }
+        // 어떤 user가 나갔는 지 room의 다른 user들에게 통보
+        socket.broadcast.to(room).emit('user_exit', {id: socket.id});
+        //console.log(users);
+        console.log("DISCONNECTED");
+    })
+});
